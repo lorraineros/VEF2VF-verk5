b997bdd2af72b87c2914d343922818c93a7d9679
Lorraine Rós <lorrainert2002@gmail.com>
2019-09-20 09:00:11 +0000

    ok

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, session, url_for
+app = Flask(__name__)
+app.config['secret_key'] = 'leyni'
+
+vorur = [   [0,"Peysa","peysa.jpg",2500],
+            [0,"Peysa","peysa.jpg",2500],
+            [0,"Peysa","peysa.jpg",2500],
+            [0,"Peysa","peysa.jpg",2500],
+            [0,"Peysa","peysa.jpg",2500],
+            [0,"Peysa","peysa.jpg",2500],
+
+        ]
+
+
+@app.route("/")
+def index():
+
+@app.route("/")
+def index():
+
+@app.route("/")
+def index():
+
+@app.route("/")
+def index():
+
+@app.route("/")
+def index():
---
+++ templates/index.tpl
@@ -0,0 +1,19 @@
+{% extends "base.html" %}
+
+{% block title %}
+    <title>Vefverslun</title>
+{% endblock %}
+
+{% block content %}
+    <h1 class="litur">Vörur</h1>
+    <section class="rapper">
+        {% for i in v %}
+            <div>
+                <h2> {{ i [1] }} </h2>
+                <img src="/static/{{ i[2] }}">
+
+                <h2>Verð: <a href="/add/{{ i[0] }}">{{ i[3] }}</a><h2>
+            </div>
+        {% endfor %}
+    </section>
+{% endblock %}
